3781cb5b1d8e6cf6f41e81476d18a56e01b53195
paul <paulbrowne2@gmail.com>
2025-02-17 19:13:13 +0000

    Updates lamdba with permissions for sagemaker, cloudwatch

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -15,12 +15,26 @@ aws cloudformation deploy \
 
 echo "Compute Stack deployed successfully."
 
+# Retrieve Compute Stack Name
+COMPUTE_STACK_OUTPUT=$(aws cloudformation describe-stacks \
+  --stack-name $COMPUTE_STACK_NAME \
+  --query "Stacks[0].StackName" \
+  --output text)
+
+if [[ -z "$COMPUTE_STACK_OUTPUT" ]]; then
+  echo "Error: Failed to retrieve Compute Stack Name."
+  exit 1
+fi
+
+echo "Compute Stack Name: $COMPUTE_STACK_OUTPUT"
+
 # Deploy Application Stack
 echo "Deploying Application Stack..."
 aws cloudformation deploy \
   --stack-name $APPLICATION_STACK_NAME \
   --template-file deploy/cloudformation/application/application.yml \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides ComputeStackName=$COMPUTE_STACK_NAME
+  --parameter-overrides ComputeStackName=$COMPUTE_STACK_OUTPUT
 
 echo "Application Stack deployed successfully."
+
